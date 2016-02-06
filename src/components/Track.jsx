18faa040f77892a82e23d3e9d7b68454b5b72ec4
Robin Wieruch <wrobin@gmx.net>
2016-02-06 01:04:57 +0100

    if not streamable

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -8,13 +8,33 @@ export default class Track extends React.Component {
     return <img src={artwork_url || avatar_url} alt={title} height='100' width='100'/>;
   }
 
+  renderOverlay(isPlaying, activity, activeTrack) {
+
+    const { origin } = activity;
+    const { stream_url } = origin;
+
+    if (!stream_url) { return; }
+
+    return (
+      <div className='track-img-overlay'>
+        <i
+            className={'fa ' + (isActivePlayingTrack(activeTrack, activity, isPlaying) ? 'fa-pause' : 'fa-play')}
+            onClick={() => this.props.activateTrack(activity)}>
+        </i>
+        <i
+            className={'fa fa-th-list'}
+            onClick={() => this.props.addTrackToPlaylist(activity)}>
+        </i>
+      </div>);
+  }
+
   renderTrack() {
     const { activity, activeTrack, isPlaying } = this.props;
     const { origin, type } = activity;
 
     if (!origin) { return; }
 
-    const { user, title, reposts_count, playback_count, likes_count, permalink_url, artwork_url } = origin;
+    const { user, title, reposts_count, playback_count, likes_count, permalink_url, artwork_url, stream_url } = origin;
     const { avatar_url, username } = user;
     const permalink_url_user = user.permalink_url;
 
@@ -22,16 +42,7 @@ export default class Track extends React.Component {
       <div className='track'>
         <div className='track-img'>
           {this.renderImage(artwork_url, title, avatar_url)}
-          <div className='track-img-overlay'>
-            <i
-                className={'fa ' + (isActivePlayingTrack(activeTrack, activity, isPlaying) ? 'fa-pause' : 'fa-play')}
-                onClick={() => this.props.activateTrack(activity)}>
-            </i>
-            <i
-                className={'fa fa-th-list'}
-                onClick={() => this.props.addTrackToPlaylist(activity)}>
-            </i>
-          </div>
+          {this.renderOverlay(isPlaying, activity, activeTrack)}
         </div>
         <div className='track-content'>
           <a href={permalink_url}><i className={getTrackIcon(type)}></i>&nbsp;{username} - {title}</a>
