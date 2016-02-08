66fc8acd7bc6232ee2839d57934ed20128a608ed
Robin Wieruch <wrobin@gmx.net>
2016-02-08 23:27:12 +0100

    scroll to top on route change

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
+import useScrollToTop from 'scroll-behavior/lib/useScrollToTop';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import { DashboardContainer } from './containers/DashboardContainer';
@@ -14,9 +15,11 @@ import App from './containers/App';
 
 require('../styles/index.scss');
 
+const history = useScrollToTop(createBrowserHistory)();
+
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <Router history={createBrowserHistory()}>
+    <Router history={history}>
       <Route path="/" component={App}>
         <Route path="/callback" component={Callback} />
         <Route path="/dashboard" component={DashboardContainer} />
