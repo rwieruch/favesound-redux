ae94f58c85b432d5055d630896189b1255105e52
Robin Wieruch <wrobin@gmx.net>
2016-03-03 20:30:43 +0100

    feat(package.json): logger

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -2,6 +2,7 @@ import createBrowserHistory from 'history/lib/createBrowserHistory'
 import {createStore, applyMiddleware} from 'redux';
 import { syncHistory, routeReducer } from 'react-router-redux'
 import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 import {combineReducers} from 'redux';
 import reducers from '../reducers/index';
 
@@ -10,8 +11,9 @@ const rootReducer = combineReducers(Object.assign({}, reducers, {
 }))
 
 const reduxRouterMiddleware = syncHistory(createBrowserHistory());
+const logger = createLogger();
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, reduxRouterMiddleware)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, reduxRouterMiddleware, logger)(createStore);
 
 export default function configureStore(initialState) {
     return createStoreWithMiddleware(rootReducer, initialState);
