177b44d535aba8ee22191933ce8137663af0aa3c
Robin Wieruch <wrobin@gmx.net>
2016-03-27 16:59:34 +0200

    feat(webpack.prod): uglify

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,36 +1,42 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        main: './src/index.jsx'
+  entry: {
+    main: './src/index.jsx'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
     },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [{
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'react-hot!babel'
-        },
-        {
-          test: /\.scss$/,
-          loader: 'style!css!sass'
-        }]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-          'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-        }),
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': '"production"'
-          }
-        })
-    ]
+    {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
+    }]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    })
+  ]
 };
