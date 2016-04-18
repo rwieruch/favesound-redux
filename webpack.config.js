2c2759afc5f6ec9c86d16cd9c5f66fb3a1a4ad12
Robin Wieruch <wrobin@gmx.net>
2016-04-18 21:27:19 +0200

    feat(*): do not use .jsx, use dev-dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.jsx'
+    './src/index.js'
   ],
   module: {
     preLoaders: [
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    main: './src/index.jsx'
+    main: './src/index.js'
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
