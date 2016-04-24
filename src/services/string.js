47cad3fda4b46eb7a470f33ef661d576a2400829
Robin Wieruch <wrobin@gmx.net>
2016-04-24 21:08:46 +0200

    feat(services/string): add string util

---
+++ src/services/string.js
@@ -0,0 +1,9 @@
+import * as requestTypes from '../constants/requestTypes';
+
+function getCommentProperty(commentId) {
+  return `${commentId}/${requestTypes.COMMENTS}`;
+}
+
+export {
+  getCommentProperty
+}
