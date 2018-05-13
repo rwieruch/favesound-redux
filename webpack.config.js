951ec1e33ab2893aab99f7972c9e29f9d042f305
Oleh T <olezhek_85@list.ru>
2018-05-13 20:47:02 +0300

    Added file-loader to load images and ico

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
-const webpack = require('webpack');
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    path.resolve(__dirname, 'src', 'index.js')
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:8080",
+    "webpack/hot/only-dev-server",
+    path.resolve(__dirname, "src", "index.js")
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
+        use: ["babel-loader", "eslint-loader"]
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: ["file-loader"]
       }
     ]
   },
   resolve: {
-    modules: [path.resolve(__dirname, 'node_modules')],
-    extensions: ['*', '.js', '.jsx']
+    modules: [path.resolve(__dirname, "node_modules")],
+    extensions: ["*", ".js", ".jsx"]
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    contentBase: path.resolve(__dirname, "dist"),
+    publicPath: "/",
     compress: true,
     hot: true,
     historyApiFallback: true
@@ -41,13 +46,14 @@ module.exports = {
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.ProvidePlugin({
-      fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+      fetch:
+        "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
     }),
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: '"development"'
       }
     })
   ],
-  devtool: 'source-map'
+  devtool: "source-map"
 };
